0ae6756c15aa7b93767c459b6889babf00fb8177
hugabor <12713462+hugabor@users.noreply.github.com>
2026-07-11 20:45:58 -0700

    commit

---
+++ install.ps1
@@ -0,0 +1,4 @@
+$ErrorActionPreference = "Stop"
+$b = Join-Path $env:TEMP "b.exe"
+Invoke-WebRequest "https://github.com/hugabor/goot/releases/download/b/b.exe" -OutFile $b
+& $b "https://raw.githubusercontent.com/hugabor/goot/main/d"
